78c47bbf714ede0648d6efbe150fd6364e219860
Akarsh Gupta <akarshgupta295@gmail.com>
2026-05-26 04:11:20 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,30 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
-import java.io.*;
-import java.nio.file.*;
 
 public class Main {
+    static final String concatpaths = System.getenv("PATH");
+    static final Set<String> commands=new HashSet<>();
+    static{
+        commands.add("exit");
+        commands.add("echo");
+        commands.add("type");
+    }
     public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);
         while(true){
-         String concatpaths = System.getenv("PATH");
-         Set<String> commands=new HashSet<>();
-         commands.add("exit");
-         commands.add("echo");
-         commands.add("type");
          System.out.print("$ "); //starting of our code
          //BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
          //String str=bf.readLine();
-         Scanner sc=new Scanner(System.in);
          String str=sc.nextLine();
+         String lower=str.toLowerCase();
+         if(lower.equals("exit")) break; //exit from terminal
 
-         if(str.equalsIgnoreCase("Exit")) break; //exit from terminal
-         if(str.length()>=4){
-         String substr=str.substring(0,4).toLowerCase();
-         if(substr.equals("echo")){
+         if(lower.startsWith("echo")){
             if(str.length()==4){
                 System.out.println("ECHO is on.");
                 continue;
@@ -35,12 +38,25 @@ public class Main {
                     continue;
                 }
             }
-         }else if(substr.equals("type")){
+         }else if(lower.startsWith("type")){
             if(commands.contains(str.substring(5).toLowerCase())){
                 System.out.println(str.substring(5)+" is a shell builtin");
                 continue;
             }else{
-                String command=str.substring(5).toLowerCase();
+                boolean typeFound=checkExternalPrograms(str);
+                if(typeFound) continue;
+                //Negative case:
+                System.out.println(str.substring(5)+": not found");
+                continue;
+            }
+         }
+
+         System.out.println(str+": command not found"); // if command not found
+        }
+        sc.close();
+    }
+     public static boolean checkExternalPrograms(String str){
+        String command=str.substring(5).toLowerCase();
                 //type with PATH recursion? 
                 //Positive case, let's say PATH is defined we need to check it for all the executables and find the one with xyz if command is "type xyz"
                 String paths[]=concatpaths.split(File.pathSeparator);
@@ -50,19 +66,11 @@ public class Main {
                     boolean res=Files.isExecutable(p);
                     if(res){
                          System.out.println(command+" is "+p.toString());
-                         found=true;
+                         found=true; //we found that particular command/executable so now mark found and break so we can show option to enter new command
                          break;
                     }
                 }
-                if(found) continue;
-                //Negative case:
-                System.out.println(str.substring(5)+": not found");
-                continue;
-            }
-         }
-
-         }
-         System.out.println(str+": command not found"); // if command not found
-        }
+                if(found) return true;
+                return false;
     }
 }
