efa5583cdc2fcf66fa35ed25bde9b7c8d67d793e
Akarsh Gupta <akarshgupta295@gmail.com>
2026-05-26 01:30:41 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
 import java.util.Scanner;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
          System.out.print("$ ");
+         BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+         String str=bf.readLine();
+         System.out.println(str+": command not found");
     }
 }
