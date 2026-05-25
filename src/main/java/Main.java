5db30a10d1fe619340b4095bbcce1d5556b21996
Akarsh Gupta <akarshgupta295@gmail.com>
2026-05-26 01:35:58 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,13 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        while(true){
          System.out.print("$ ");
          //BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
          //String str=bf.readLine();
          Scanner sc=new Scanner(System.in);
          String str=sc.nextLine();
          System.out.println(str+": command not found");
+        }
     }
 }
